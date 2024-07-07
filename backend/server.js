5ea26482e6b1faed2568a77b7c6bc12c7253ea74
Miit Daga <miitcodes27@gmail.com>
2024-07-07 10:54:23 +0530

    pushed backend part for login and register

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require('cookie-parser');
+
+connectDB();
+const app = express();
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173"
+  ],
+  "Access-Control-Allow-Origin": ["http://localhost:5173"],
+  "Access-Control-Allow-Credentials": true,
+  methods: "GET,POST,PUT,DELETE,PATCH",
+  optionsSuccessStatus: 200,
+  allowedHeaders: ["Content-Type", "Authorization","set-cookie"],
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+// Ensure your app handles preflight requests
+app.options('*', cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "welcome to the quiz - api!" });
+});
+
+app.use(authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
+});
