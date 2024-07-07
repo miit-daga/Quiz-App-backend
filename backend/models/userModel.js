08988ed1f76ba2195b4e866c2f531b1abccb1451
Miit Daga <miitcodes27@gmail.com>
2024-07-08 00:55:33 +0530

    made minor fixes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -43,4 +43,11 @@ const userSchema = mongoose.Schema(
   }
 );
 
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 module.exports = mongoose.model('User', userSchema);
